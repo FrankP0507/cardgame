dc61d726cb51d589b7378488891399725accfeaa
Perez <fperez@FVFZMYJ3J1WK.tld>
2022-04-22 08:19:04 -0700

    cardgame

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 function run() {
     getCard()
     stop()
+    ct()
 }
 
 function getCard() {
@@ -10,7 +11,12 @@ function getCard() {
     document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
     
 }
-
+function computer() {
+    var r = Math.floor(Math.random() * 10) + 1
+    var suitArray = ["C", "D", "H", "S"]
+    var randomSuit = Math.floor(Math.random() * 4 )// 0 to 3
+    document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
+}
 
 function stop() {
     
