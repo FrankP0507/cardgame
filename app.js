96519655ca23bb9f7d09965fb1a2277f42afc894
Adell <jadell@FVFY2F9HJ1WK.tld>
2022-04-25 10:56:28 -0500

    balls

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+function run() {
+    getCard()
+}
+
 var human = 0;
 var computer = 0;
 
@@ -7,25 +11,24 @@ function getCard() {
     var suitArray = ["C", "D", "H", "S"]
     var randomSuit = Math.floor(Math.random() * 4 )// 0 to 3
     document.getElementById("card").insertAdjacentHTML("beforeend", + "class='cards2'" + "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
-    }while (human < 20);
+    human = human + r
+    }while (human <= 20);
 }
-alert("The computer will now go.")
+
 
 function ct() {
+    alert("The computer will now go.")
     do {
     var r = Math.floor(Math.random() * 10) + 1
     var suitArray = ["C", "D", "H", "S"]
     var randomSuit = Math.floor(Math.random() * 4 )// 0 to 3
     document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
-    }while (computer < 20);
+    computer = computer + r
+    }while (computer <= 20);
 }
 
 function Restart() {
     location.reload()
-}
-
-function stop() {
-    $("#card").hide();
 }
 
 function game() {
@@ -56,5 +59,4 @@ function game() {
         }
     }
 }
-
 
