191083bd3ab00a4662a4b237479e612605cb6212
Perez <fperez@FVFZMYJ3J1WK.tld>
2022-04-25 08:11:13 -0700

    cardgame

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 function run() {
     getCard()
     stop()
-    ct()
 }
 
+var human = 0;
+var computer = 0;
+
 function getCard() {
+    do {
     var r = Math.floor(Math.random() * 10) + 1
     var suitArray = ["C", "D", "H", "S"]
     var randomSuit = Math.floor(Math.random() * 4 )// 0 to 3
     document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
-    
+    }while (human < 20);
 }
+
+function ct() {
+    do {
+    var r = Math.floor(Math.random() * 10) + 1
+    var suitArray = ["C", "D", "H", "S"]
+    var randomSuit = Math.floor(Math.random() * 4 )// 0 to 3
+    document.getElementById("card").insertAdjacentHTML("beforeend", "<img src=images/" + r + suitArray[randomSuit] + ".jpg>")
+    }while (computer < 20);
+}
+
+
 function Restart() {
     location.reload()
 }
